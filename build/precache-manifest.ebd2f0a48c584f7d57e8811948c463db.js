230c29ffae226d72680595452e90a0776589772a
xieping <cdxping@163.com>
2020-09-17 10:54:14 +0800

    init

---
+++ build/precache-manifest.ebd2f0a48c584f7d57e8811948c463db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b653c8fe95707e9d80359b90f6c15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0ec2e46ed68ef63cb4",
+    "url": "/static/css/main.a7a801a8.chunk.css"
+  },
+  {
+    "revision": "cee55c8d30d6199779ad",
+    "url": "/static/js/2.787ff227.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.787ff227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0ec2e46ed68ef63cb4",
+    "url": "/static/js/main.cb88ef8f.chunk.js"
+  },
+  {
+    "revision": "1d35f4335ab5c185b8ea",
+    "url": "/static/js/runtime-main.df3a21c2.js"
+  }
+]);
